14f4dbc1d4e4a1aa07bb6c392dd470f3b2d86785
Torfiks <101979863+Torfiks@users.noreply.github.com>
2025-10-09 01:30:20 +0300

    move to common/service LoadDataService.kt

--- app/src/main/java/ru/ushell/app/data/common/service/LoadDataService.kt
+++ app/src/main/java/ru/ushell/app/data/common/service/LoadDataService.kt
@@ -1,17 +1,19 @@
-package ru.ushell.app.data.features
+package ru.ushell.app.data.common.service
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import jakarta.inject.Inject
 import kotlinx.coroutines.launch
+import ru.ushell.app.data.features.attendance.AttendanceRepository
 import ru.ushell.app.data.features.timetabel.TimetableRepository
 import ru.ushell.app.data.features.user.UserRepository
 
 @HiltViewModel
 class LoadDataService@Inject constructor(
     private val userRepository: UserRepository,
-    private val timetableRepository: TimetableRepository
+    private val timetableRepository: TimetableRepository,
+    private val attendanceRepository: AttendanceRepository,
 ): ViewModel() {
 
     fun loadData(){
@@ -21,6 +23,7 @@ class LoadDataService@Inject constructor(
                 if (!userRepository.activeUser()) return@launch
 
                 timetableRepository.saveTimetable()
+                attendanceRepository.saveAttendance()
 
             } catch (e: Exception) {
                 println("Error loading timetable: $e")
