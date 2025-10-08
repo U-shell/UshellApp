c34d6b62f9b6dae3d79a38c2589ed4cb7bf481b1
Torfiks <101979863+Torfiks@users.noreply.github.com>
2025-10-08 15:28:27 +0300

    add new fun timetable dao

--- app/src/main/java/ru/ushell/app/base/UserDatabase.kt
+++ app/src/main/java/ru/ushell/app/base/UserDatabase.kt
@@ -2,16 +2,26 @@ package ru.ushell.app.base
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import ru.ushell.app.data.features.timetabel.room.dao.main.TimetableDao
+import ru.ushell.app.data.features.timetabel.room.dao.main.TimetableEntity
+import ru.ushell.app.data.features.timetabel.room.dao.secondary.TimetableSecondaryDao
+import ru.ushell.app.data.features.timetabel.room.dao.secondary.TimetableSecondaryEntity
 import ru.ushell.app.data.features.user.room.dao.UserDao
 import ru.ushell.app.data.features.user.room.dao.UserEntity
 
 @Database(
     entities = [
-        UserEntity::class
-    ], version = 2, exportSchema = true
+        UserEntity::class,
+        TimetableEntity::class,
+        TimetableSecondaryEntity::class
+    ], version = 11, exportSchema = true
 )
 abstract class UserDatabase : RoomDatabase() {
 
     abstract fun userDao(): UserDao
+
+    abstract fun timetableDao(): TimetableDao
+
+    abstract fun timetableSecondaryDao(): TimetableSecondaryDao
 
 }
