51803f3be497e17c2ed3f7575f83d54f25cbe422
Torfiks <101979863+Torfiks@users.noreply.github.com>
2026-02-03 21:14:18 +0300

    update gradel | lib & clear code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,10 +1,7 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.compose.core)
-    alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.room)
     alias(libs.plugins.ksp)
     alias(libs.plugins.hilt)
@@ -12,7 +9,12 @@ plugins {
 
 android {
     namespace = "ru.ushell.app"
-    compileSdk = 36
+
+    compileSdk {
+        version = release(36)
+    }
+
+    enableKotlin = false
 
     defaultConfig {
         applicationId = "ru.ushell.app"
@@ -20,6 +22,7 @@ android {
         targetSdk = 36
         versionCode = 7
         versionName = "5.0.6"
+
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -32,6 +35,7 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -39,16 +43,6 @@ android {
 
     buildFeatures {
         compose = true
-    }
-
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.4"
-    }
-}
-
-kotlin {
-    compilerOptions {
-        jvmTarget.set(JvmTarget.JVM_11)
     }
 }
 
@@ -59,44 +53,45 @@ room {
 dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-    implementation(libs.androidx.compose.animation)
-    implementation(libs.androidx.compose.foundation)
-    testImplementation(libs.junit)
+
 
     implementation(libs.constraintlayout.compose)
     implementation(libs.accompanist.swiperefresh)
 
     implementation(libs.material3)
+    implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.material3.window.size.class1)
     implementation(libs.androidx.material3.adaptive.navigation.suite)
 
-    implementation(libs.androidx.navigation.compose)
 
-    implementation(libs.androidx.camera.lifecycle)
-    implementation(libs.androidx.camera.camera2)
     implementation(libs.androidx.camera.core)
     implementation(libs.androidx.camera.view)
-
-    implementation(libs.room.runtime)
-    implementation(libs.room.sqlite)
-    implementation(libs.room.sqlite.bundled)
-    ksp(libs.room.compiler)
-
-    implementation(libs.hilt.android)
-    implementation(libs.androidx.hilt.navigation.compose)
-    implementation(libs.dagger)
-
-    ksp(libs.hilt.android.compiler)
-
-    implementation(libs.retrofit)
-    implementation(libs.logging.interceptor)
-    implementation(libs.gson)
-    implementation(libs.converter.gson)
-
-    implementation(libs.zxing.android.embedded)
+    implementation(libs.androidx.camera.camera2)
+    implementation(libs.androidx.camera.lifecycle)
 
     implementation(libs.coil.compose)
     implementation(libs.coil.network.okhttp)
+    implementation(libs.dagger)
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.android.compiler)
+    implementation(libs.androidx.hilt.compiler)
+    implementation(libs.androidx.hilt.navigation.compose)
+
+    implementation(libs.androidx.compose.animation)
+    implementation(libs.androidx.compose.foundation)
+    implementation(libs.androidx.compose.foundation.layout)
+    implementation(libs.androidx.foundation.layout)
+    implementation(libs.zxing.android.embedded)
+
+    ksp(libs.room.compiler)
+    implementation(libs.room.runtime)
+    implementation(libs.gson)
+    implementation(libs.converter.gson)
+
+    implementation(libs.retrofit)
+    implementation(libs.logging.interceptor)
+
+    testImplementation(libs.junit)
 
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.ui.test.junit4)
